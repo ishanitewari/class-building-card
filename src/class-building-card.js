dffdda883f00257a9762de9b929808b1a2b955c9
Sobarzo14 <abelardo.sobarzo.14@gmail.com>
2023-03-21 12:27:49 -0400

    Fix image

--- src/class-building-card.js
+++ src/class-building-card.js
@@ -32,22 +32,29 @@ class ClassBuildingCard extends LitElement {
       'https://ist.psu.edu/sites/default/files/featured/visitor-info.jpg';
     this.description =
       "The Westgate building is located on the west end of Penn State’s University Park campus in University Park, Pennsylvania, housing the College of Information Sciences and Technology. Inside the building you'll find 199,000 square-feet of classroom spaces and gathering spaces, for studying, meetings, or just hanging out.";
-    this.top = 'I hate this place';
+    this.top = 'Westgate';
     this.details = 'The IST Powerhouse of the cell';
-    this.accentColor =
-      'linear-gradient(180.3deg, rgb(221, 221, 221) 5.5%, rgb(110, 136, 161) 90.2%)';
+    this.accentColor = 'blue';
     this.openDetails = false;
   }
 
   static styles = css`
+    :host([accent-color='blue']) .card {
+      background-color: #bef0ff;
+    }
+    :host([accent-color='coral']) .card {
+      background-color: #ffbdbd;
+    }
+    :host([accent-color='green']) .card {
+      background-color: #01d086;
+    }
+    :host([accent-color='orange']) .card {
+      background-color: #f39b59;
+    }
     .card {
-      background: linear-gradient(
-        180.3deg,
-        rgb(221, 221, 221) 5.5%,
-        rgb(110, 136, 161) 90.2%
-      );
       border-radius: 2%;
       padding: 15px;
+      width: 600px;
     }
     .title {
       font-family: Georgia;
@@ -55,13 +62,14 @@ class ClassBuildingCard extends LitElement {
       text-align: center;
       padding: 8px;
     }
-    meme-maker {
+    .inside-card meme-maker {
       width: 50%;
     }
     .inside-card {
       background-color: lightGray;
       margin: 8px;
       display: flex;
+      flex-direction: row;
       border: 6px #32a1ce;
       border-radius: 7px;
     }
@@ -139,10 +147,6 @@ class ClassBuildingCard extends LitElement {
       }
     }
     @media screen and (min-width: 800px) {
-      .card {
-        width: 800px;
-      }
-
       .title {
         color: grey;
         text-align: center;
@@ -169,15 +173,14 @@ class ClassBuildingCard extends LitElement {
       <div class="card">
         <h1 class="title">${this.name}</h1>
         <section class="inside-card">
-        <meme-maker img="${this.url}" top-text="${this.top}"></meme-maker>
-          <img class="building" src="${this.img}"  alt="${this.name}">
+          <meme-maker img="${this.img}" top-text="${this.top}"></meme-maker>
           <div class="not-image">
-          <p class="description">${this.description}</p>
-                    <slot></slot>
-                <details class="details" .open="${this.openDetails}">
-                <p class="description"></p>
-            <a class="haxButton" href="#">Details </a>
-          </div>
+            <p class="description">${this.description}</p>
+                  <slot></slot>
+                  <details class="details" .open="${this.openDetails}">
+                  <p class="description"></p>
+              <a class="haxButton" href="#">Details </a>
+            </div>
         </section>
       </div>
     `;
